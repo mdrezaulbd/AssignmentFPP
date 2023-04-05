3d8ae081eb6cad3fb0ba9c5a6dfa7e8226814970
rkarimbd <mdrezaulkhan@gmail.com>
2023-04-05 15:50:39 -0500

    assignment -2 problem-4

--- src/assignment2/Prog4.java
+++ src/assignment2/Prog4.java
@@ -4,15 +4,15 @@ public class Prog4 {
 
     public static void main(String[] args) {
 
-        float f1=1.27f,f2=3.881f,f3=9.6f;
+        float f1 = 1.27f, f2 = 3.881f, f3 = 9.6f;
 
-        int sum=(int)(f1+f2+f3);
+        int sum = (int) (f1 + f2 + f3);
 
-        int sumround=(int)Math.round(f1+f2+f3);
+        int sumround = (int) Math.round(f1 + f2 + f3);
 
-        System.out.println("the sum of the floats as an integer, obtained by casting the sum to type int :"+sum);
+        System.out.println("the sum of the floats as an integer, obtained by casting the sum to type int :" + sum);
         System.out.println("\n");
-        System.out.println("the sum of the floats as an integer, using the Math.round function : :"+sumround);
+        System.out.println("the sum of the floats as an integer, using the Math.round function : :" + sumround);
 
     }
 }
